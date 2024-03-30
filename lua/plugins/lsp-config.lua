0d6bd4b7573f5a97e6e8653e67f53b8b60aa1d4a
Austin Eisele <austine@austin@aeiselecloud.com>
2024-03-29 20:47:44 -0400

    adding rust lsp

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,63 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({
+				PATH = "prepend",
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			--lua lsp set up
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.rust_analyzer.setup({
+				settings = {
+					["rust-analyzer"] = {
+						diagnostics = {
+							enable = false,
+						},
+					},
+				},
+			})
+
+			--Omnisharp set up
+			local pid = vim.fn.getpid()
+			local omnisharp_bin =
+				"C:\\Users\\aeisele\\AppData\\Local\\nvim-data\\mason\\packages\\omnisharp\\omnisharp.cmd"
+			lspconfig.omnisharp.setup({
+				cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+				enable_editorconfig_support = true,
+				enable_ms_build_load_projects_on_demand = false,
+				enable_rosyln_analzyers = true,
+				root_dir = lspconfig.util.root_pattern("*.csproj", "*.sln"),
+				capabilities = capabilities,
+				handlers = {
+					["textDocument/definition"] = require("omnisharp_extended").handler,
+				},
+			})
+
+			--keybindinga
+			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
